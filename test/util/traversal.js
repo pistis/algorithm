bd28304ea7dbe51bee8ea8f79fb12c026f3af470
범희 이 <vamalboro@gmail.com>
2015-07-03 22:43:21 +0900

    update traversal binary tree testcode

--- test/util/traversal.js
+++ test/util/traversal.js
@@ -3,30 +3,25 @@ var Node = require('../../src/js/util/binary-tree-node');
 var Traversal = require('../../src/js/util/traversal');
 
 describe('traversal', function() {
-
-    var tempTree = function() {
-        var root = new Node('1');
+    var tree = null;
+    beforeEach(function() {
+        tree = new Node('1');
         var a = new Node('2');
         var b = new Node('3');
         var c = new Node('4');
         var d = new Node('5');
         var e = new Node('6');
         var f = new Node('7');
-        root.left = a;
-        root.right = b;
+        tree.left = a;
+        tree.right = b;
         a.left = c;
         a.right = d;
         b.left = e;
         b.right = f;
-        return root;
-    };
-
-    beforeEach(function() {
-
     });
 
     afterEach(function() {
-
+        tree = null;
     });
 
     it('should have a working test environment', function() {
@@ -34,38 +29,32 @@ describe('traversal', function() {
     });
 
     it('should tree traversal by preorder', function () {
-        var node = tempTree();
-
         var result = '';
         var traversal = new Traversal().setVisit(function(data){
             result += data;
         });
 
-        traversal.preOrder(node);
+        traversal.preOrder(tree);
         result.should.equal('1245367');
     });
 
     it('should tree traversal by inorder', function () {
-        var node = tempTree();
-
         var result = '';
         var traversal = new Traversal().setVisit(function(data){
             result += data;
         });
 
-        traversal.inOrder(node);
+        traversal.inOrder(tree);
         result.should.equal('4251637');
     });
 
     it('should tree traversal by postorder', function () {
-        var node = tempTree();
-
         var result = '';
         var traversal = new Traversal().setVisit(function(data){
             result += data;
         });
 
-        traversal.postOrder(node);
+        traversal.postOrder(tree);
         result.should.equal('4526731');
     });
 
